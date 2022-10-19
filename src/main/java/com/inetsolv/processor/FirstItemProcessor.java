817ab8081d9bfba9b14823351ab0dc3a8aa60e0a
ramesh <rameshjava4plus@gmail.com>
2022-10-19 22:03:52 +0530

    springbatch

---
+++ src/main/java/com/inetsolv/processor/FirstItemProcessor.java
@@ -0,0 +1,15 @@
+package com.inetsolv.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstItemProcessor implements ItemProcessor<Integer, Long> {
+
+	@Override
+	public Long process(Integer item) throws Exception {
+		System.out.println("Inside Item Processor");
+		return Long.valueOf(item + 20);
+	}
+
+}
